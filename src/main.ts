76ac7f8383fa4fa04d50055436157f8a017835b6
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2025-01-22 12:20:33 +0530

    renames app to sketch

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import p5 from 'p5';
 import mySketch from './sketch';
 
-const elem = document.getElementById('app') || undefined;
+const elem = document.getElementById('sketch') || undefined;
 new p5(mySketch, elem);
